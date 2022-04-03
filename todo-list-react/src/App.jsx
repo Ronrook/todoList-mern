b0846de419e618694f7577d7f4ea7c97bc052dd1
Ronald Rosero <ronaldes93@gmail.com>
2022-04-03 16:20:34 -0500

    create components

--- todo-list-react/src/App.jsx
+++ todo-list-react/src/App.jsx
@@ -1,43 +1,21 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Form from './components/Form';
+import Section from './components/Section';
+const appTitle = "TO - DO - APP";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="ui container center aligned">
+        <Section>
+            <h1>{appTitle}</h1>
+        </Section>
+
+        <Section>
+            <Form/>
+        </Section>
+      
     </div>
   )
 }
